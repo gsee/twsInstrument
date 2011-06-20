d372e4a3fb0e7542700ef689661c94689bd36d2a
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-20 23:11:45 +0000

    -add plotInstruments
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/twsInstrument@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/plotRelPerf.R
+++ R/plotRelPerf.R
@@ -12,3 +12,17 @@ plotRelPerf <- function(symbols,prefer=NULL, env=.GlobalEnv) {
 #getSymbols(symbols)
 #plotRelPerf(symbols)
 
+
+plotInstruments <- function(symbols,prefer=NULL, env=.GlobalEnv) {
+    x <- xts()
+    if (length(symbols) <= 1) stop('vector of symbol names required')    
+    for (symbol in symbols) { 
+        x <- cbind(x,getPrice(na.omit(get(symbol,pos=env)),prefer=prefer))
+    }
+    ts.plot(x,col=rainbow(NCOL(x)))
+}
+
+#plotInstruments(c("VIX_JAN11","VIX_FEB11","VIX_MAR11","VIX_APR11"))
+
+
+
