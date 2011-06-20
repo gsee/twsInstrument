228e74165bd31725d65979c3c109fbcbd66bad93
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-20 05:52:58 +0000

    -loadInstruments: instruments are now added to existing environment instead of creating a new .instrument environment.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/twsInstrument@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/saveInstruments.R
+++ R/saveInstruments.R
@@ -5,9 +5,16 @@ saveInstruments <- function(file_name="MyInstruments", dir="", extension="RData"
 	save(.instrument,file=paste(dir,file_name,".",extension,sep=""))	
 }
 
-loadInstruments <-function(file_name="MyInstruments", dir="", extension="RData", env=parent.frame()) {
+loadInstruments <-function(file_name="MyInstruments", dir="", extension="RData", env=.GlobalEnv) {
 	if (!is.null(dir) && !dir == "" && substr(dir,nchar(dir),nchar(dir)) != "/")
 		dir <- paste(dir,"/",sep="")
-	load(paste(dir,file_name,".",extension,sep=""),envir=env)
+    tmpenv <- new.env()
+	load(paste(dir,file_name,".",extension,sep=""),envir=tmpenv)
+    .instrument <- get(".instrument",pos=env)
+    il <- ls(tmpenv$.instrument,all.names=TRUE)
+    for (i in il) {
+        .instrument[[i]] <- tmpenv$.instrument[[i]]
+    }
+    assign(".instrument",.instrument, pos=env)
 }
 
