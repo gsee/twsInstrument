7bb8443c9eb234f3ccd814707a3643fe9c7630d8
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-05 14:45:22 +0000

    loop through symbols as well as expiries in define_futures().
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/twsInstrument@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/define_futures.R
+++ R/define_futures.R
@@ -1,12 +1,14 @@
-define_futures.IB <- function(symbol, exch, expiries=as.Date(Sys.time()), currency="USD", include_expired = "0", ...) {
+define_futures.IB <- function(symbols, exch, expiries=as.Date(Sys.time()), currency="USD", include_expired = "0", ...) {
     symout <- NULL    
     for (expiry in expiries) {
-        symout <- c(symout, twsInstrument(twsFUT(symbol, exch, expiry, currency=currency, include_expired=include_expired, ...),output='symbol'))
+        for (symbol in symbols) {        
+            symout <- c(symout, twsInstrument(twsFUT(symbol, exch, expiry, currency=currency, include_expired=include_expired, ...),output='symbol'))
+        }    
     }
     symout
 }
 
-define_futures  <- function(symbol, exch, expiries=as.Date(Sys.time()), currency="USD", include_expired="0", src='IB', ...) {
-    do.call(paste("define_futures.",src,sep=""),list(symbol=symbol,exch=exch,expiries=expiries, currency=currency, include_expired=include_expired, ...))
+define_futures  <- function(symbols, exch, expiries=as.Date(Sys.time()), currency="USD", include_expired="0", src='IB', ...) {
+    do.call(paste("define_futures.",src,sep=""),list(symbols=symbols,exch=exch,expiries=expiries, currency=currency, include_expired=include_expired, ...))
 }
 
