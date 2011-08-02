0d3f8522ecf1c3a8a41a0248f5ba4610e68ef677
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-02 21:38:37 +0000

    add twsClock utility.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/twsInstrument@109 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/twsClock.R
@@ -0,0 +1,40 @@
+#' A clock utility to display the TWS server time
+#'
+#' adapted from the code in the links in references
+#' @param format format
+#' @param refresh refresh rate
+#' @param verbose be verbose about what clientId you are connected with?
+#' @usage twsClock(format = "\%H:\%M:\%S", refresh = 1, verbose=TRUE)
+#' @return live updating current time as reported by \code{reqCurrentTime}. Press escape to exit clock utility.
+#' @references
+#' http://stackoverflow.com/questions/5953718/overwrite-current-output-in-the-r-console
+#' http://4dpiecharts.com/2011/05/11/a-clock-utility-via-console-hackery/
+#' http://www.asciitable.com/
+#' @examples
+#' \dontrun{
+#' twsClock()
+#' twsClock("%A %d %B %Y %I:%M:%OS3 %p", 1e-3)
+#' }
+twsClock <- function(format = "%H:%M:%S", refresh = 1, verbose=TRUE)
+{
+    tryCatch({
+            tws <- try(twsConnect(1010))
+        if (inherits(tws, "try-error")) 
+            tws <- try(twsConnect(1011))
+        if (inherits(tws, "try-error")) 
+            tws <- twsConnect(9999)
+        if (isConnected(tws) && verbose) 
+            cat(paste("Connected with clientId", tws$clientId, '\n'))
+        if (tws$clientId == 9999) 
+            warning("IB TWS should be restarted.")    
+        repeat
+        {
+            cat("\r", format(reqCurrentTime(tws), format), sep = "")
+            flush.console()
+            Sys.sleep(refresh)
+        }
+    }, finally = {
+        twsDisconnect(tws)
+    })
+}
+
