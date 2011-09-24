9d01ba4d40a48e93f81fb0339bd0239f798e9242
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-24 15:26:11 +0000

    more work on ls_instruments_by
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/twsInstrument@132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/ls_instruments_by.R
+++ R/ls_instruments_by.R
@@ -18,18 +18,19 @@ ls_instruments_by <- function (what, value, pattern=NULL, match=TRUE, in.slot=NU
         tmp_instr <- try(get(symbol, pos = .instrument),silent=TRUE)
         #TODO: clean this up
         if (is.instrument(tmp_instr)) {
-            if (is.null(value)) {
-                if (!is.null(tmp_instr[[what]])) {
-                    tmp_symbols <- c(tmp_symbols,symbol)        
-                } else if (!inherits(try(tmp_instr[[in.slot]][[what]],silent=TRUE),'try-error') && !is.null(tmp_instr[[in.slot]][[what]])) tmp_symbols <- c(tmp_symbols,symbol)
-            } else if (!is.null(in.slot) && !inherits(try(tmp_instr[[in.slot]][[what]],silent=TRUE),'try-error') && !is.null(tmp_instr[[in.slot]][[what]]) && any(tmp_instr[[in.slot]][[what]] == value)) {
-                tmp_symbols <- c(tmp_symbols,symbol)
-            } else if (!is.null(tmp_instr[[what]]) && any(tmp_instr[[what]] == value)){    
-                tmp_symbols <- c(tmp_symbols,symbol)
-            }
+            if (
+                if (is.null(value)) {
+                    if (is.null(in.slot)) {
+                        if (!inherits(try(tmp_instr[[what]],silent=TRUE), 'try-error') && !is.null(tmp_instr[[what]])) {TRUE} else {FALSE}
+                    } else if (!inherits(try(tmp_instr[[in.slot]][[what]],silent=TRUE), 'try-error') && !is.null(tmp_instr[[in.slot]][[what]])) {TRUE} else {FALSE}
+                } else if (is.null(in.slot)) {
+                    if (!is.null(tmp_instr[[what]]) ) {TRUE} else {FALSE}
+                } else { #!is.null(value) && !is.null(in.slot)
+                    if (!is.null(tmp_instr[[in.slot]][[what]])) {TRUE} else {FALSE}
+                }
+            ) tmp_symbols <- c(tmp_symbols, symbol)
         }    
     }
     tmp_symbols
 }
-
 
