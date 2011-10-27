700d6f1fecc2549a2e3f7be3e81c16edc2faefb8
gsee <gsee@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-27 18:07:59 +0000

    - cannot drop workaround for include_expires until IBrokers 0.9-5 is pushed to CRAN  - put back code in getContract that I thought was unnecessary, but turns out to have a purpose.  - update package-level doc to indicate which clientIds getContract uses.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/twsinstrument/pkg/twsInstrument@187 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getContract.R
+++ R/getContract.R
@@ -54,16 +54,45 @@
 getContract <- function(x, verbose=TRUE, silent=FALSE, ...) {
     #if (is.xts(x)) x <- deparse(substitute(x))
     if (inherits(x, 'twsContractDetails')) return(x$contract)
+    if (inherits(x, 'eventPortfolioValue')) return(x$contract)
     instr <- if (is.instrument(x)) {x} else try(getInstrument(x, silent=TRUE))
     if (is.twsInstrument(instr)) return(instr$IB)
     if (is.character(x)) { #IBrokers method of getSymbols implies there may be a 'twsContract' stored in Symbol Lookup table.
         SL <- getSymbolLookup()[[x]]
         if (!is.null(SL$Contract) && SL$src == 'IBrokers')
             if (is.twsContract(SL$Contract)) return(SL$Contract)   
-    }        
-    return(Contr_From_Instr(x, assign_c=FALSE, verbose=verbose, silent=silent, ...))
+    }
+    tmpnum <- try(suppressWarnings(as.numeric(x)), silent=TRUE)
+    if (any(is.na(tmpnum))) return(Contr_From_Instr(x, assign_c=FALSE, verbose=verbose, silent=silent, ...))
+    else {
+        tryCatch({
+            tryCatch(
+            {
+                if ( (hasArg(tws) && is.twsConnection(tws) && !isConnected(tws))
+                    || !hasArg(tws)) {tws <- try(ConnectIB(c(110:114, 150)), silent=TRUE)}
+            }, finally={
+                if (isConnected(tws)) {                
+                    if (verbose) 
+                        cat(paste('Connected with clientId ', tws$clientId, '.\n',sep=""))    
+                    if (tws$clientId == 150) warning("IB Trader Workstation should be restarted.")                    
+                    contract <- twsContract()
+                    contract$conId <- paste(x)
+                    contract$include_expired <- "1" #reqContractDetails will overwrite this (due to a bug it has), 
+                                                    #but you need it here in case this contract is expired. 
+                    #request that IB fill in missing info.
+                    details <- try(suppressWarnings(reqContractDetails(tws,contract)),silent=TRUE)
+                    if (!inherits(details,'try-error') && length(details)) {
+                        details <- details[[1]]        
+                    } else details <- NULL
+                } else if (verbose) cat('Could not connect to tws.')
+            }) #end nested tryCatch  
+        },finally=twsDisconnect(tws)) #End outer tryCatch
+        if (is.null(details)) {
+            stop("Could not find twsContract with conId ", x)
+        } else if (verbose) cat('Contract details request complete. Disconnected.\n')
+        return(details$contract)
+    }
 }
-
 #' Remove local copies of twsContracts
 #'
 #' Remove any local copies of a twsContract so that
